24e35bfc39b09589205a87b825ca817a4076a48c
brunosas88 <bruno.sas@live.com>
2022-08-05 09:08:11 -0300

    feat: utilização de anotações lombok e criação de metodo para converter um modelo em um dto como reposta para uma requisição dessa classe

--- src/main/java/me/dio/academiadigital/matricula/dto/RespostaMatricula.java
+++ src/main/java/me/dio/academiadigital/matricula/dto/RespostaMatricula.java
@@ -1,12 +1,28 @@
 package me.dio.academiadigital.matricula.dto;
 
+import lombok.*;
+import me.dio.academiadigital.aluno.dto.AlunoDTO;
 import me.dio.academiadigital.aluno.model.Aluno;
+import me.dio.academiadigital.matricula.model.Matricula;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
 
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class RespostaMatricula {
     private UUID matriculaId;
     private LocalDateTime dataDaMatricula;
-    private Aluno aluno;
+    private AlunoDTO aluno;
+
+    public static RespostaMatricula converterParaDTO(Matricula model) {
+        return RespostaMatricula.builder()
+                .matriculaId(model.getMatriculaId())
+                .dataDaMatricula(model.getDataDaMatricula())
+                .aluno(AlunoDTO.converterParaDTO(model.getAluno()))
+                .build();
+    }
 }
