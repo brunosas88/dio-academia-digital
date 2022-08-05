74216cd5a0cf43c74337322eae8fbd03aeb70c74
brunosas88 <bruno.sas@live.com>
2022-08-05 13:57:00 -0300

    feat: dto para atualizar dados de endereço do aluno

---
+++ src/main/java/me/dio/academiadigital/aluno/dto/AlunoAtualizacaoDeDadosDTO.java
@@ -0,0 +1,12 @@
+package me.dio.academiadigital.aluno.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class AlunoAtualizacaoDeDadosDTO {
+    private String endereco;
+}
