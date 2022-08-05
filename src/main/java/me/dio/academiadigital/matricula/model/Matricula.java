ab06167940206b7f127c97a6b769e2c9ac096f19
brunosas88 <bruno.sas@live.com>
2022-08-05 08:56:30 -0300

    refactor: Atualização para mudança de tipo de id e adição de relacionamento com classe Aluno

--- src/main/java/me/dio/academiadigital/matricula/model/Matricula.java
+++ src/main/java/me/dio/academiadigital/matricula/model/Matricula.java
@@ -1,23 +1,32 @@
 package me.dio.academiadigital.matricula.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import me.dio.academiadigital.aluno.model.Aluno;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
 public class Matricula {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private UUID id;
-    @Column
+    private Integer id;
+
     private UUID matriculaId;
 
-    @Column
     private LocalDateTime dataDaMatricula = LocalDateTime.now();
 
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "aluno_id")
     private Aluno aluno;
 
 }
