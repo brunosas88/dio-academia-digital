2f3df9bd1e845b60d226144d232e4960851c5c8d
brunosas88 <bruno.sas@live.com>
2022-08-05 08:42:42 -0300

    refactor: Atualização da classe Aluno com mapeamento de relação, adição de constraint e metodo estático para conversão entre modelo e dto

--- src/main/java/me/dio/academiadigital/aluno/model/Aluno.java
+++ src/main/java/me/dio/academiadigital/aluno/model/Aluno.java
@@ -1,34 +1,58 @@
 package me.dio.academiadigital.aluno.model;
 
+import lombok.*;
+import me.dio.academiadigital.aluno.dto.AlunoDTO;
 import me.dio.academiadigital.avaliacaofisica.model.AvaliacaoFisica;
-import me.dio.academiadigital.matricula.model.Matricula;
 
 import javax.persistence.*;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.UUID;
+import java.util.Objects;
 
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 @Entity(name = "aluno")
 public class Aluno {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private UUID id;
-    @Column
+    private Integer id;
+
     private String nome;
-    @Column
+
+    @Column(unique = true)
     private String cpf;
-    @Column
+
     private String endereco;
-    @Column
+
     private LocalDate dataDeNascimento;
 
-    List<AvaliacaoFisica> avaliacoes = new ArrayList<>();
+    @OneToMany(mappedBy = "aluno", fetch = FetchType.LAZY)
+    List<AvaliacaoFisica> avaliacoes;
 
+    public static Aluno converterParaModelo (AlunoDTO dto) {
+        return Aluno.builder()
+                .nome(dto.getNome())
+                .cpf(dto.getCpf())
+                .endereco(dto.getEndereco())
+                .dataDeNascimento(dto.getDataDeNascimento())
+                .avaliacoes(new ArrayList<>())
+                .build();
+    }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Aluno aluno = (Aluno) o;
+        return Objects.equals(id, aluno.id) && Objects.equals(nome, aluno.nome) && Objects.equals(cpf, aluno.cpf) && Objects.equals(endereco, aluno.endereco) && Objects.equals(dataDeNascimento, aluno.dataDeNascimento) && Objects.equals(avaliacoes, aluno.avaliacoes);
+    }
 
-
-
-
-
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, nome, cpf, endereco, dataDeNascimento, avaliacoes);
+    }
 }
