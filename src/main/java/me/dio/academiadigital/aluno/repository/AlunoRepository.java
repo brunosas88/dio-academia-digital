77e3e282e76d53ec966e64137dc4c8ac93e56b9f
brunosas88 <bruno.sas@live.com>
2022-08-05 08:44:58 -0300

    feat: Adicionado repositorio da classe Aluno

---
+++ src/main/java/me/dio/academiadigital/aluno/repository/AlunoRepository.java
@@ -0,0 +1,16 @@
+package me.dio.academiadigital.aluno.repository;
+
+import me.dio.academiadigital.aluno.model.Aluno;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+
+@Repository
+public interface AlunoRepository extends JpaRepository<Aluno, Integer>, JpaSpecificationExecutor<Aluno> {
+
+    Aluno findByCpf(String cpf);
+
+}
