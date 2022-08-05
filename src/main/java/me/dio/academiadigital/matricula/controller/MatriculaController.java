a68a24ddf1bf5944d9fb2e1d8804f4ccd383934b
brunosas88 <bruno.sas@live.com>
2022-08-05 10:40:32 -0300

    feat: adicionado endpoint para deletar matricula e consequentemente o aluno

--- src/main/java/me/dio/academiadigital/matricula/controller/MatriculaController.java
+++ src/main/java/me/dio/academiadigital/matricula/controller/MatriculaController.java
@@ -7,9 +7,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.lang.Nullable;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
@@ -21,5 +19,10 @@ public class MatriculaController {
     @GetMapping
     public ResponseEntity<Page<RespostaMatricula>> listarMatriculas(@Nullable Pageable pageable) {
         return ResponseEntity.ok(matriculaService.listarMatriculas(pageable));
+    }
+
+    @DeleteMapping("/deletar")
+    public void deletarMatricula(@RequestParam(value = "cpf") String cpf ) {
+        matriculaService.deletarMatricula(cpf);
     }
 }
