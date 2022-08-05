39b6790fadf9e82dab3a7846d3cbf2a600974a90
brunosas88 <bruno.sas@live.com>
2022-08-05 08:55:23 -0300

    fix: Correção de importação de classes

--- src/main/java/me/dio/academiadigital/infra/jackson/deser/LocalDateDeserializer.java
+++ src/main/java/me/dio/academiadigital/infra/jackson/deser/LocalDateDeserializer.java
@@ -3,7 +3,7 @@ package me.dio.academiadigital.infra.jackson.deser;
 import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.databind.DeserializationContext;
 import com.fasterxml.jackson.databind.deser.std.StdDeserializer;
-import me.dio.academia.digital.infra.utils.JavaTimeUtils;
+import me.dio.academiadigital.infra.utils.JavaTimeUtils;
 
 import java.io.IOException;
 import java.time.LocalDate;
--- src/main/java/me/dio/academiadigital/infra/jackson/ser/LocalDateTimeSerializer.java
+++ src/main/java/me/dio/academiadigital/infra/jackson/ser/LocalDateTimeSerializer.java
@@ -3,7 +3,7 @@ package me.dio.academiadigital.infra.jackson.ser;
 import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.databind.SerializerProvider;
 import com.fasterxml.jackson.databind.ser.std.StdSerializer;
-import me.dio.academia.digital.infra.utils.JavaTimeUtils;
+import me.dio.academiadigital.infra.utils.JavaTimeUtils;
 
 import java.io.IOException;
 import java.time.LocalDateTime;
